8eef14095003b7b2489b36c6ba6162f58fa4f252
Chad_Hackney <hackney.chad@gmail.com>
2019-04-10 00:49:45 -0500

    bedtime 9Apr19

--- prepare_zillow.py
+++ prepare_zillow.py
@@ -14,25 +14,36 @@ def singles_only(df):
     df = df[df.propertylandusedesc.isin(singles)] 
     return df
 
+def nums_to_obj(df):
+    df[['parcelid', 'buildingqualitytypeid', 'fips', 'rawcensustractandblock', 'regionidcity', 'regionidcounty', 'regionidzip', 'yearbuilt', 'assessmentyear', 'censustractandblock']] = df[['parcelid', 'buildingqualitytypeid', 'fips', 'rawcensustractandblock', 'regionidcity', 'regionidcounty', 'regionidzip', 'yearbuilt', 'assessmentyear', 'censustractandblock']].astype(str)
+    return df
+
 def remove_columns(df, cols_to_remove):  
     df = df.drop(columns=cols_to_remove)
     return df
 
-# def remove_columns(get_full_zillow, cols_to_remove):  
-# df = get_full_zillow.drop(columns=cols_to_remove)
-# return df
-
-def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
+def handle_missing_values(df, prop_required_column = .75, prop_required_row = .75):
     threshold = int(round(prop_required_column*len(df.index),0))
     df.dropna(axis=1, thresh=threshold, inplace=True)
     threshold = int(round(prop_required_row*len(df.columns),0))
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
 
-def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75):
+# def fill_unitcnt(df):
+#     df['unitcnt'] = df['unitcnt'].fillna(value=0, inplace = True)
+#     return df
+
+# def fill_all_others(df):
+#     df = df.fillna(value=0, inplace = True)
+#     return df
+
+def data_prep(df, cols_to_remove=['heatingorsystemdesc','lotsizesquarefeet','propertyzoningdesc','fullbathcnt'], prop_required_column=.75, prop_required_row=.75):
+    df = nums_to_obj(df)
     df = singles_only(df)
     df = remove_columns(df, cols_to_remove)
     df = handle_missing_values(df, prop_required_column, prop_required_row)
+    # df = fill_unitcnt(df)
+    # df = fill_all_others(df)
     return df
 
 def peekatdata(df):
